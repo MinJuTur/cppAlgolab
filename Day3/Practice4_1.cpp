674d8f95a0f72a21b0364b209fb4c2992ba5e577
MinJuTur <jmj4701jmj@gmail.com>
2025-04-06 10:26:01 +0900

    Algolab Day3-4번 수정

--- Day3/Practice4_1.cpp
+++ Day3/Practice4_1.cpp
@@ -5,13 +5,12 @@ using namespace std;
 int calculateEndDay(int year, int month) {
   if (month == 2) {
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) return 29;
-    return 28;
-  }
-  if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
+    else return 28;
+  } else if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
   return 31;
 }
 
-int calculateWeek(int year, int month, int day) {
+int calculateDayOfWeek(int year, int month, int day) {
   if (month == 1 || month == 2) {
     year--;
     month += 12;
@@ -19,6 +18,7 @@ int calculateWeek(int year, int month, int day) {
 
   int k = year % 100;
   int j = year / 100;
+  // 0 = 토요일, 1 = 일요일, ..., 6 = 금요일
   int result = (day + 13*(month+1)/5 + k + k/4 + j/4 + 5*j) % 7;
 
   // 0 = 일요일, 1 = 월요일, ..., 6 = 토요일로 변환
@@ -37,20 +37,20 @@ int main() {
 
     cout << Y << " " << M << "\n";
 
-    int startWeek = calculateWeek(Y, M, 1);
+    int startDayOfWeek = calculateDayOfWeek(Y, M, 1);
     int endDay = calculateEndDay(Y, M);
-    int num = 1;
+    int Day = 1;
     while (true) {
       for (int j = 0; j < 7; j++) {
         if (j > 0) cout << " ";
 
-        if (--startWeek >= 0) cout << 0;
-        else if (num > endDay) cout << 0;
-        else cout << num++;
+        if (--startDayOfWeek >= 0) cout << 0;
+        else if (Day > endDay) cout << 0;
+        else cout << Day++;
       }
       cout << "\n";
 
-      if (num > endDay) break;
+      if (Day > endDay) break;
     }
   }
 }
